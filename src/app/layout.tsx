cbc72ce1d65b7dc1940887e9194eb6a4df98ad82
Thyme68 <thongtqse173366@fpt.edu.vn>
2026-07-18 18:51:46 +0700

    Adopt Gilroy typography and stack support details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,14 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700&display=swap&subset=vietnamese,latin-ext"
-          rel="stylesheet"
-        />
-      </head>
       <body>
         <LanguageProvider>
           <BrokenImageGuard />
